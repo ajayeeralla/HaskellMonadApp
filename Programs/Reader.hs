83edf9e0c622f03d90b59121dd79eb26a768c218
Ajay Eeralla <ajay.eeralla@gmail.com>
2022-05-12 14:45:03 -0700

    done with first exercise

---
+++ Programs/Reader.hs
@@ -0,0 +1,34 @@
+module Reader
+    ( WeatherConditions(..)
+    , main
+    )
+where
+import Control.Monad.Reader
+import Data.AST
+
+
+data WeatherConditions =
+        WeatherConditions
+        { temperature :: Int
+        , temperatureScale :: TemperatureScale
+        , weatherStatus :: WeatherStatus
+        }
+
+weatherConditionString :: Reader WeatherConditions String
+weatherConditionString = do
+                        weather <- ask
+                        let temp = temperature weather
+                            scale = temperatureScale weather
+                            status = weatherStatus weather
+                        return $ "The current weather conditions are "++ show status++ ", " ++ show temp ++ " degrees " ++ show scale
+
+wc = WeatherConditions
+        { temperature = 53
+        , temperatureScale = Fahrenheit
+        , weatherStatus = Clouds
+        }
+
+main :: IO ()
+main =  do
+        let str = runReader weatherConditionString wc
+        putStrLn str
