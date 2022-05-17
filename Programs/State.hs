74b3cce22ae2b36e502d485b8259164178944841
Ajay Eeralla <ajay.eeralla@gmail.com>
2022-05-17 14:31:04 -0700

    complete State exercise

--- Programs/State.hs
+++ Programs/State.hs
@@ -1,73 +1,65 @@
 module State where
-import Control.Monad.State.Lazy (State, execState)
-import Control.Monad.State.Class ( MonadState(get, put))
-import Text.ParserCombinators.ReadP (count)
---import GHC.Show (Show)
---import GHC.Stack.CCS (ccLabel)
+import Control.Monad.State.Lazy
+  ( State
+  , execState
+  )
+import Control.Monad.State.Class
+  ( MonadState(get, put)
+  )
 
 data Color =
-        Red
-    |   Green
-    |   Blue
+    Red
+    | Green
+    | Blue
     deriving(Show)
 
 data ColorCounts =
-        ColorCounts
-            { redCount :: Int
-            , greenCount :: Int
-            , blueCount :: Int
-            }
-
-setColorCountsZero :: ColorCounts
-setColorCountsZero =
     ColorCounts
-        { redCount = 0
-        , greenCount = 0
-        , blueCount = 0
-        }
+    { redCount :: Int
+    , greenCount :: Int
+    , blueCount :: Int
+    }
+
+incRed :: ColorCounts -> ColorCounts
+incRed cc =
+    let x = redCount cc in
+    cc {redCount=x+1}
+
+incGreen :: ColorCounts -> ColorCounts
+incGreen cc =
+    let x = greenCount cc in
+    cc {greenCount=x+1}
+
+incBlue :: ColorCounts -> ColorCounts
+incBlue cc =
+    let x = blueCount cc in
+    cc {blueCount=x+1}
+
+count :: [Color] -> State ColorCounts ()
+count [] = do
+    _ <- get
+    return ()
+count (c:cs) = do
+    cc <- get
+    case c of
+        Red -> put $ incRed cc
+        Green -> put $ incGreen cc
+        Blue -> put $ incBlue cc
+    count cs
+
+defaultColorCounts :: ColorCounts
+defaultColorCounts =
+    ColorCounts
+    { redCount = 0
+    , greenCount = 0
+    , blueCount = 0
+    }
 
 listColors :: [Color]
 listColors = [Red, Green, Blue, Red, Blue]
 
-countRed :: [Color] -> Int
-countRed lc =
-    case lc of
-        [] -> 0
-        h:t -> case h of
-                Red -> 1 + countRed t
-                _   -> countRed t
-
-countGreen :: [Color] -> Int
-countGreen lc =
-    case lc of
-        [] -> 0
-        h:t -> case h of
-                Green -> 1 + countGreen t
-                _     ->  countGreen t
-
-countBlue :: [Color] -> Int
-countBlue lc =
-    case lc of
-        [] -> 0
-        h:t -> case h of
-                Blue -> 1 + countBlue t
-                _    -> countBlue t
-
-
-countColors :: State ColorCounts ()
-countColors = do
-              cc <- get
-              let rc = countRed listColors
-                  gc = countGreen listColors
-                  bc = countBlue listColors
-              put cc{redCount=rc, greenCount=gc, blueCount=bc}
---             --   let rc = redCount cc
---             --       gc = greenCount cc
---             --       bc = blueCount cc
---               --return cc
-
 main :: IO()
 main = do
-       let cc = setColorCountsZero
-           s = execState countColors cc
-       putStrLn $ "redcount = " ++ show (redCount s) ++ ", greenCount = " ++ show (greenCount s) ++ ", blueCount = " ++ show (blueCount s)
+    let state = count listColors
+    let s = execState state defaultColorCounts
+    print $ "redcount = " ++ show (redCount s) ++ ", greenCount = " ++ show (greenCount s) ++ ", blueCount = " ++ show (blueCount s)
